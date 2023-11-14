7a2f53e2140a36198fe79265a5418b44a2675fce
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-14 09:33:38 +0100

    setup: Finish the Department entity class

--- src/api/Domain/Entities/Department.cs
+++ src/api/Domain/Entities/Department.cs
@@ -5,7 +5,37 @@ namespace Domain.Entities;
 
 public class Department : Entity<DepartmentId>
 {
-    private Department(DepartmentId id) : base(id)
+    public string Title { get; private set; }
+    public string Code { get; private set; }
+    public AcademicManager? Manager { get; private set; }
+    private Department(
+        DepartmentId id,
+        string title,
+        string code,
+        AcademicManager? manager) : base(id)
     {
+        Code = code;
+        Title = title;
+        Manager = manager;
+    }
+
+    public static Department CreateUnique(
+        string title,
+        string code,
+        AcademicManager? manager
+    )
+    {
+        return new Department(
+            new DepartmentId(Guid.NewGuid()),
+            title,
+            code,
+            manager
+        );
+    }
+
+    public void AssignManager(AcademicManager manager)
+    {
+        ArgumentNullException.ThrowIfNull(manager);
+        Manager = manager;
     }
 }
