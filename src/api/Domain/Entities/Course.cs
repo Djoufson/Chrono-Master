b1263cdca6fc54fd22da36d2541abc125ca727cc
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-14 09:38:48 +0100

    setup: Finish the Course entity class

--- src/api/Domain/Entities/Course.cs
+++ src/api/Domain/Entities/Course.cs
@@ -5,7 +5,30 @@ namespace Domain.Entities;
 
 public class Course : Entity<CourseId>
 {
-    public Course(CourseId id) : base(id)
+    public Department Department { get; private set; }
+    public string Title { get; set; }
+    public int TotalHours { get; set; }
+
+    private Course(
+        CourseId id,
+        string title,
+        int totalHours,
+        Department department) : base(id)
     {
+        Title = title;
+        TotalHours = totalHours;
+        Department = department;
+    }
+
+    public static Course CreateUnique(
+        string title,
+        int totalHours,
+        Department department)
+    {
+        return new(
+            new CourseId(Guid.NewGuid()),
+            title,
+            totalHours,
+            department);
     }
 }
