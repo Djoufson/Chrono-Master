b0382543c09391d8972c6d49b869fec47e0a1524
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-14 09:58:04 +0100

    setup: Finish PlanningDefinition entity class

--- src/api/Domain/Entities/DefinitionItem.cs
+++ src/api/Domain/Entities/DefinitionItem.cs
@@ -5,7 +5,46 @@ namespace Domain.Entities;
 
 public class DefinitionItem : Entity<DefinitionItemId>
 {
-    private DefinitionItem(DefinitionItemId id) : base(id)
+    public DayOfWeek DayOfWeek { get; private set; }
+    public TimeOnly StartTime { get; private set; }
+    public TimeSpan Duration { get; private set; }
+    public PlanningDefinition Definition { get; set; }
+
+    private DefinitionItem(
+        DefinitionItemId id,
+        PlanningDefinition definition,
+        DayOfWeek dayOfWeek,
+        TimeOnly startTime,
+        TimeSpan duration) : base(id)
     {
+        DayOfWeek = dayOfWeek;
+        StartTime = startTime;
+        Duration = duration;
+        Definition = definition;
+    }
+
+    public static DefinitionItem CreateUnique(
+        PlanningDefinition definition,
+        DayOfWeek dayOfWeek,
+        TimeOnly startTime,
+        TimeSpan duration)
+    {
+        return new(
+            new DefinitionItemId(Guid.NewGuid()),
+            definition,
+            dayOfWeek,
+            startTime,
+            duration
+        );
+    }
+
+    public void UpdateDefinitionItem(
+        DayOfWeek dayOfWeek,
+        TimeOnly startTime,
+        TimeSpan duration)
+    {
+        DayOfWeek = dayOfWeek;
+        StartTime = startTime;
+        Duration = duration;
     }
 }
--- src/api/Domain/Entities/PlanningDefinition.cs
+++ src/api/Domain/Entities/PlanningDefinition.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using Domain.Entities.Base;
 using Domain.ValueObjects;
 
@@ -5,7 +6,29 @@ namespace Domain.Entities;
 
 public class PlanningDefinition : Entity<PlanningDefinitionId>
 {
-    public PlanningDefinition(PlanningDefinitionId id) : base(id)
+    public ICollection<DefinitionItem> Definitions { get; private set; } = new Collection<DefinitionItem>();
+    private PlanningDefinition(
+        PlanningDefinitionId id) : base(id)
     {
+    }
+
+    public static PlanningDefinition CreateUnique()
+    {
+        return new(
+            new PlanningDefinitionId(Guid.NewGuid())
+        );
+    }
+
+    public void AddDefinition(DefinitionItem definition)
+    {
+        if(Definitions.Any(d => d.Id == definition.Id))
+            return;
+
+        Definitions.Add(definition);
+    }
+
+    public void RemoveDefinition(DefinitionItem definition)
+    {
+        Definitions.Remove(definition);
     }
 }
